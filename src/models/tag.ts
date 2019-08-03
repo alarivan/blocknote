9b6f531e4504eeb11e219d4f870690ef4fbc99aa
alarivan <alarivan@pm.me>
2019-08-03 15:55:36 -0700

    Add Tag interface

--- src/models/tag.ts
+++ src/models/tag.ts
@@ -1,13 +1,20 @@
 import randomColor from "randomcolor";
 import uuidv4 from "uuid/v4";
 
+export interface Tag {
+  id: string;
+  name: string;
+  color: string | string[];
+}
+
 export const NAME_ERROR_MESSAGE = "Name can't be empty";
 
-const generateColor = (color: string | undefined) =>
+const generateColor = (color: string | undefined): string | string[] =>
   color || randomColor({ luminosity: "light" });
+
 const formatName = (name: string) => name.trim().toLowerCase();
 
-const TagModel = function(name: string, color?: string) {
+const TagModel = function(name: string, color?: string): Tag {
   if (!name) {
     throw NAME_ERROR_MESSAGE;
   }
