ae8915b4d8c27b64a685a517d42e418d39060493
alarivan <alarivan@pm.me>
2019-08-05 17:50:58 -0700

    Enable redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,18 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+declare global {
+  interface Window {
+    // eslint-disable-next-line no-undef
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
