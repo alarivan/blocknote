b15ec33ced765e0831f189f5ee89826440781473
alarivan <alarivan@pm.me>
2019-08-06 19:58:41 -0700

    Update rootReducer type

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,12 +1,21 @@
+import { Reducer } from "redux";
 import tagsReducer from "./tags/reducer";
 import notesReducer from "./notes/reducer";
+import { NotesActionTypes } from "store/notes/types";
+import { TagsActionTypes } from "store/tags/types";
+import { EmptyAction } from "store/actions";
 import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({
+const _rootReducer = combineReducers({
   tagsState: tagsReducer,
   notesState: notesReducer
 });
 
-export type AppState = ReturnType<typeof rootReducer>;
+export type AppState = ReturnType<typeof _rootReducer>;
+
+const rootReducer: Reducer<
+  AppState,
+  NotesActionTypes | TagsActionTypes | EmptyAction
+> = _rootReducer;
 
 export default rootReducer;
